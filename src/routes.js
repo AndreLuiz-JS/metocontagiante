24f43058ce2f6e885d27be0544d316256c9f8cff
AndreLuiz-JS <oxxxandreluizxxxo@gmail.com>
2020-04-15 23:25:41 -0300

    Adicionada a rota para pagina de login e admin

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Bible from './pages/bible';
 import Maps from './pages/maps';
 import Podcast from './pages/podcast';
 import Devotional from './pages/devotional';
+import Login from './pages/login';
+import Admin from './pages/admin';
 
 const Routes = () => (
     <Switch>
@@ -13,6 +15,9 @@ const Routes = () => (
         <Route path="/maps" component={Maps} />
         <Route path="/podcast" component={Podcast} />
         <Route path="/devotional" component={Devotional} />
+        <Route path="/login" component={Login} />
+
+        <Route path="/admin" component={Admin} />
     </Switch>
 )
 
