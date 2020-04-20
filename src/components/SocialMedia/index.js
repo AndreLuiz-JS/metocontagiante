3b46b09ca9341fc85ae8b4df222a2471bda68a08
AndreLuiz-JS <oxxxandreluizxxxo@gmail.com>
2020-04-20 15:07:36 -0300

    Adicionado tags para segurança na abertura de links target blank

--- src/components/SocialMedia/index.js
+++ src/components/SocialMedia/index.js
@@ -7,8 +7,8 @@ import { Container } from './styles';
 export default function SocialMedia() {
     return (
         <Container>
-            <a target="_blank" href="http://fb.me/metocontagiante"><FiFacebook size={30} /></a>
-            <a target="_blank" href="http://instagram.com/metodistacontagiante"><FiInstagram size={30} /></a>
+            <a target="_blank" rel="noopener noreferrer" href="http://fb.me/metocontagiante"><FiFacebook size={30} /></a>
+            <a target="_blank" rel="noopener noreferrer" href="http://instagram.com/metodistacontagiante"><FiInstagram size={30} /></a>
         </Container>
     )
 }
