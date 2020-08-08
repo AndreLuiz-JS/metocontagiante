d0f7a33e11d4ca6d0fbf92c559c5ea8ade6179db
AndreLuiz-JS <oxxxandreluizxxxo@gmail.com>
2020-08-08 14:40:06 -0300

    Refactoring: change Loading component to typescript

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Spinner from "react-spinkit";
+import { Load } from "./styles";
+
+import theme from "../../styles/themes/default";
+
+export default function Loading({ loading, message, ico = "cube-grid" }) {
+	return loading ? (
+		<Load>
+			<Spinner name={ico} fadeIn="none" color={theme.colors.effect} />
+			<span className="message">{message}</span>
+		</Load>
+	) : null;
+}
